116a9417baad63bbaf9b33bade35ac48b129c4e9
Vishal__Dev <mylaptop800mrj@gmail.com>
2023-07-28 16:48:03 +0530

    useMemo with example

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import ParentComponent from './UseCallback/ParentComponent'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+     <ParentComponent ></ParentComponent>
+    </>
+  )
+}
+
+export default App
